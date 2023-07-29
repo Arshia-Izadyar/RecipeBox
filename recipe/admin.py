ff36cef9d7506c2538c4cf860390edc6d534842c
Arshia-Izadyar <arshiaa104@gmail.com>
2023-07-29 09:09:14 +0330

    added recipe

---
+++ recipe/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Category, Comment, Like, Favorite, Recipe
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "parent")
+    
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created_time")
+    search_fields = ("title", "user")
+    order_by = ("-created_time")
+    
---
+++ recipe/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+    
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name=_("Name"))
+    parent = models.ForeignKey('self', related_name="children", on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+class Recipe(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name=_("Created time"))
+    modified_time = models.DateTimeField(verbose_name=_("Modified time"))
+    title = models.CharField(max_length=150, verbose_name=_("Title"))
+    description = models.TextField(blank=True, null=True, verbose_name=_("Description"))
+    instructions = models.TextField(verbose_name=_("Instructions"))
+    time_to_cook = models.DurationField(verbose_name=_("time to cook"))
+    image = models.ImageField(upload_to="images/", verbose_name=_("Image"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe", verbose_name=_("User"))
+    # like = models.PositiveBigIntegerField(default=0, )
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="categories", verbose_name=_("Category"))
+    
+    def __str__(self):
+        return f"{self.title} - {self.created_time}"
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE, verbose_name=_("User"))
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments", verbose_name=_("Recipe"))
+    content = models.TextField(verbose_name=_("Content"))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=_("Created date"))
+    text = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user} - {self.created_date}"
+    
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name="likes", on_delete=models.CASCADE, verbose_name=_("User"))
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="likes", verbose_name=_("Recipe"))
+    class Meta:
+        unique_together = ('user', 'recipe')
+        
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, related_name="favorites", on_delete=models.CASCADE, verbose_name=_("User"))
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="favorites", verbose_name=_("Recipe"))
+    
+    class Meta:
+        verbose_name = _('Favorite')
+        verbose_name_plural = _('Favorites')
+        unique_together = ('user', 'recipe')
+        
+    def __str__(self):
+        return f"{self.user} - {self.recipe}"
+    
